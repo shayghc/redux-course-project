2f43b4595b6d5f62f1de5f72e173ba7e38973b1e
Seamus Connolly <sghconnolly@gmail.com>
2018-09-13 14:23:08 +0100

    feat: Add createStore function and state variable

---
+++ index.js
@@ -0,0 +1,10 @@
+function createStore () {
+    // The store should have four parts:
+    // 1. The state
+    // 2. Get the state
+    // 3. Listen to the state
+    // 4. Update the state
+
+
+    let state
+}
