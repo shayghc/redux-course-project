55fe6d8cdede49ed379f395ef8f49395b59e177a
Seamus Connolly <sghconnolly@gmail.com>
2018-09-17 08:34:35 +0100

    feat: COmbine reducers

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
-{
-    type: 'ADD_TODO',
-    todo: {
-        id: 0,
-        name: 'Learn Redux',
-        complete: false
-    }
-}
-
-{
-    type: 'REMOVE_TODO',
-    id: 0,
-}
-
-{
-    type: 'TOGGLE_TODO',
-    id: 0,
-}
-
-{
-    type: 'ADD_GOAL',
-    goal: {
-        id: 0,
-        name: 'Run a marathon'
-    }
-}
-
-{
-    type: 'REMOVE_GOAL',
-    id: 0
-}
-
 /*
     Characteristics of a pure function
     1. They always return the same result if the same arguments are passed in.
     2. They depend only upon the arguments passed into them.
     3. The never produce any side-effects.
 */
-// Reducer function
+
+// Reducer function - for TODOS
 function todos(state = [], action) {
     switch(action.type) {
         case 'ADD_TODO':
@@ -51,7 +20,26 @@ function todos(state = [], action) {
     }
 }
 
-function createStore (reducer) {
+// Reducer function - for GOALS
+function goals(state = [], action) {
+    switch(action.type) {
+        case 'ADD_GOAL':
+            return state.concat([action.goal]);
+        case 'REMOVE_GOAL':
+            return state.filter((goal) => goal.id !== action.id);
+        default:
+            return state;
+    }
+}
+
+function app(state = {}, action) {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
+
+function createStore(reducer) {
     // The store should have four parts:
     // 1. The state
     // 2. Get the state - getState
@@ -83,14 +71,67 @@ function createStore (reducer) {
     }
 }
 
-const store = createStore(todos)
+const store = createStore(app)
+
+store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+})
 
 // This function will "dispatch an action"
 store.dispatch({
     type: 'ADD_TODO',
     todo: {
         id: 0,
-        name: 'Learn Redux',
-        complete: false
+        name: 'Walk the dog',
+        complete: false,
     }
+})
+
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 1,
+        name: 'Wash the car',
+        complete: false,
+    }
+})
+
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 2,
+        name: 'Go to the gym',
+        complete: true,
+    }
+})
+
+store.dispatch({
+    type: 'REMOVE_TODO',
+    id: 1
+})
+
+store.dispatch({
+    type: 'TOGGLE_TODO',
+    id: 0
+})
+
+store.dispatch({
+    type: 'ADD_GOAL',
+    goal: {
+        id: 0,
+        name: 'Learn Redux'
+    }
+})
+
+store.dispatch({
+    type: 'ADD_GOAL',
+    goal: {
+        id: 1,
+        name: 'Lose 20 pounds'
+    }
+})
+
+store.dispatch({
+    type: 'REMOVE_GOAL',
+    id: 0
 })
